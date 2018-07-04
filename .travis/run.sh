5d9e2894e314c5b5bf3f7d8798b7f4f526888d41
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-07-04 09:59:52 -0300

    modularized env var and files management

--- .travis/run.sh
+++ .travis/run.sh
@@ -10,16 +10,25 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
     pyenv activate conan
 fi
 
-python ci_utils/print_version.py
+# python ci_utils/print_version.py
+# export BITPRIM_BUILD_NUMBER="$(python ci_utils/print_version.py)"
+# export BITPRIM_CONAN_VERSION="${BITPRIM_BUILD_NUMBER}"
+# # echo "${BITPRIM_BUILD_NUMBER}"
+# # echo "${BITPRIM_CONAN_VERSION}"
 
-export BITPRIM_BUILD_NUMBER="$(python ci_utils/print_version.py)"
-export BITPRIM_CONAN_VERSION="${BITPRIM_BUILD_NUMBER}"
-# echo "${BITPRIM_BUILD_NUMBER}"
-# echo "${BITPRIM_CONAN_VERSION}"
+# if [ ! -f conan_version ]; then
+#     echo "Creating conan_version file"
+#     printf "${BITPRIM_BUILD_NUMBER}" > conan_version
+# fi
 
-if [ ! -f conan_version ]; then
-    echo "Creating conan_version file"
-    printf "${BITPRIM_BUILD_NUMBER}" > conan_version
-fi
+python ci_utils/set_envvars_and_files.py
+
+echo $BITPRIM_CONAN_VERSION
+echo $BITPRIM_CONAN_CHANNEL
+echo $BITPRIM_BRANCH
+echo $BITPRIM_FULL_BUILD
+echo $BITPRIM_BUILD_NUMBER
+cat conan_version
+
 
 python build.py
---
+++ set_envvars_and_files.py
@@ -0,0 +1,44 @@
+import os
+from utils import get_version
+
+# cd ${TRAVIS_BUILD_DIR}
+# git fetch --unshallow
+
+if os.environ.has_key('TRAVIS_BRANCH'):
+    branch = os.environ.get('TRAVIS_BRANCH')
+elif os.environ.has_key('APPVEYOR_REPO_BRANCH'):
+    branch = os.environ.get('APPVEYOR_REPO_BRANCH')
+
+os.environ['BITPRIM_BRANCH'] = branch
+
+if branch == 'dev':
+    os.environ['BITPRIM_CONAN_CHANNEL'] = "testing"
+    os.environ['BITPRIM_FULL_BUILD'] = 0
+elif branch.startswith('release'):
+    os.environ['BITPRIM_CONAN_CHANNEL'] = "prerelease"
+    os.environ['BITPRIM_FULL_BUILD'] = 1
+elif branch.startswith('hotfix'):
+    os.environ['BITPRIM_CONAN_CHANNEL'] = "prerelease"
+    os.environ['BITPRIM_FULL_BUILD'] = 1
+elif branch.startswith('feature'):
+    os.environ['BITPRIM_CONAN_CHANNEL'] = branch
+    os.environ['BITPRIM_FULL_BUILD'] = 0
+else:
+    os.environ['BITPRIM_CONAN_CHANNEL'] = "prerelease"
+    os.environ['BITPRIM_FULL_BUILD'] = 1
+
+# export BITPRIM_CONAN_VERSION="$(python ci_utils/print_version.py)"
+version = get_version()
+os.environ['BITPRIM_CONAN_VERSION'] = version
+os.environ['BITPRIM_BUILD_NUMBER'] = version
+
+
+# if [ ! -f conan_version ]; then
+#     echo "Creating conan_version file"
+#     printf "${BITPRIM_BUILD_NUMBER}" > conan_version
+# fi
+
+if not os.path.exists('conan_version'):
+    print("Creating conan_version file")
+    with open("conan_version", "w") as file:
+        file.write(version)
