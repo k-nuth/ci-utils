d24c72c9eb93bdd8da5f3c163fc8bd9b41bb9d40
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-07-18 09:42:15 -0300

    git describe master

--- utils.py
+++ utils.py
@@ -38,7 +38,7 @@ def get_git_branch(default=None):
 
 def get_git_describe(default=None):
     try:
-        res = subprocess.Popen(["git", "describe"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        res = subprocess.Popen(["git", "describe", "master"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, _ = res.communicate()
         if output:
             if res.returncode == 0:
