ac17bed5656c2b3ec9cb5f08ede1c8e8430dc02d
Fernando Pelliccioni <fpelliccioni@gmail.com>
2018-08-15 19:27:14 -0400

    temp 8

--- .travis/run.sh
+++ .travis/run.sh
@@ -15,7 +15,8 @@ eval $(python ci_utils/set_envvars_and_files.py)
 echo $CONAN_CHANNEL
 
 conan remote add bitprim https://api.bintray.com/conan/bitprim/bitprim
-conan info . --only None
+# conan info . --only None
+conan info . --only None -s compiler=gcc -s compiler.version=5 -s compiler.libcxx=libstdc++
 python ci_utils/process_conan_reqs.py
 conan remote remove bitprim
 
--- process_conan_reqs.py
+++ process_conan_reqs.py
@@ -5,7 +5,11 @@ import fileinput
 def get_conan_info(default=None):
     try:
         # conan info . --only None  
-        params = ["conan", "info", ".", "--only", "None"]
+        # params = ["conan", "info", ".", "--only", "None"]
+
+        # conan info . --only None -s compiler=gcc -s compiler.version=5 -s compiler.libcxx=libstdc++
+        params = ["conan", "info", ".", "--only", "None", "-s", "compiler=gcc", "-s", "compiler.version=5", "-s", "compiler.libcxx=libstdc++"]
+
         res = subprocess.Popen(params, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         output, _ = res.communicate()
         if output:
