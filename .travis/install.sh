8ca0e8aa69b78daa3877d6612fb2da655bf53da2
Fernando Pelliccioni <fpelliccioni@gmail.com>
2019-04-11 10:15:43 -0300

    Using Py3 on macOS

--- .travis/install.sh
+++ .travis/install.sh
@@ -13,16 +13,24 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
         eval "$(pyenv init -)"
     fi
 
-    pyenv install 2.7.10
-    pyenv virtualenv 2.7.10 conan
+#     pyenv install 2.7.10
+#     pyenv virtualenv 2.7.10 conan
+    pyenv install 3.7.1
+    pyenv virtualenv 3.7.1 conan
     pyenv rehash
     pyenv activate conan
 fi
 
-# pip install conan --upgrade > /dev/null
-pip install conan==1.7.4 > /dev/null
+pip install  --upgrade pip > /dev/null
+# pip install  --upgrade conan > /dev/null
+# pip install conan==1.7.4 > /dev/null
+pip install conan==1.14.1 > /dev/null
 pip install conan_package_tools > /dev/null
 pip install cpuid --upgrade > /dev/null
 
+
 conan --version
 conan user
+
+
+
