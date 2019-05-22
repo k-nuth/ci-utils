15bb3dc1580b8e2f3371340137fb40c9b01cb523
Fernando Pelliccioni <fpelliccioni@gmail.com>
2019-05-22 16:38:26 -0300

    rebranding

--- cmake/BitprimTools.cmake
+++ cmake/BitprimTools.cmake
@@ -1,7 +1,7 @@
 #
 # Copyright (c) 2016-2019 Knuth Project.
 #
-# This file is part of K-nuth Project.
+# This file is part of Knuth Project.
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
