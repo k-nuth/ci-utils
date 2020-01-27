2e0362ecce79ad273eecee95389a2777d9b1655e
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-01-27 18:44:38 -0300

    using kthbuild

--- .travis/install.sh
+++ .travis/install.sh
@@ -23,11 +23,13 @@ fi
 
 pip install  --upgrade pip > /dev/null
 
-pip install conan==1.19.2 > /dev/null
+# pip install conan==1.19.2 > /dev/null
 pip install conan_package_tools==0.29.3 > /dev/null
-pip install cpuid --upgrade > /dev/null
-# pip install  --upgrade conan > /dev/null
+# pip install cpuid --upgrade > /dev/null
+# # pip install  --upgrade conan > /dev/null
 
+# pip install kthbuild==0.0.14 > /dev/null
+pip install kthbuild --upgrade > /dev/null 
 
 conan --version
 conan user
