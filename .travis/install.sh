f1b6d44276d9d94621f734aa383e937d9249c299
Fernando Pelliccioni <fpelliccioni@gmail.com>
2020-09-24 13:28:46 -0300

    using latest version of CPT

--- .travis/install.sh
+++ .travis/install.sh
@@ -21,8 +21,7 @@ fi
 
 pip install  --upgrade pip > /dev/null
 
-# pip install conan_package_tools==0.30.4 > /dev/null
-pip install conan_package_tools==0.32.4 > /dev/null
+pip install conan_package_tools==0.34.2 > /dev/null
 # pip install kthbuild==0.0.14 > /dev/null
 pip install kthbuild --upgrade > /dev/null 
 
