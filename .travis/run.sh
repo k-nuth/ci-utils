2655e0906b5e6fe1febbf4bf702ebd8af7d5c7a6
Fernando Pelliccioni <fpelliccioni@gmail.com>
2021-04-12 15:29:11 -0300

    list remotes

--- .travis/run.sh
+++ .travis/run.sh
@@ -14,8 +14,16 @@ echo $CONAN_CHANNEL
 eval $(python ci_utils/set_envvars_and_files.py)
 echo $CONAN_CHANNEL
 
+conan user
+conan remote list
+
 conan remote add kth https://knuth.jfrog.io/artifactory/api/conan/knuth || true
 conan remote add kth https://knuth.jfrog.io/artifactory/api/conan/knuth || true
+
+
+conan user
+conan remote list
+
 # conan info . --only None
 # conan info . --only None -s compiler=gcc -s compiler.version=5 -s compiler.libcxx=libstdc++
 python ci_utils/process_conan_reqs.py
